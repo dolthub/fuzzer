9a2770e81b95c8b47bfe49a0db3dc4771d7e1a27
Daylon Wilkins <daylon@liquidata.co>
2021-08-02 15:38:29 -0700

    Changed CHAR to always generate fixed-length strings

--- types/char.go
+++ types/char.go
@@ -18,7 +18,6 @@ import (
 	"fmt"
 	"math"
 	"strings"
-	"unicode"
 
 	"github.com/dolthub/go-mysql-server/sql"
 
@@ -56,53 +55,47 @@ func (c *Char) Instance() (TypeInstance, error) {
 	if err != nil {
 		return nil, errors.Wrap(err)
 	}
-	return &CharInstance{ranges.NewInt([]int64{0, charLength}), collation}, nil
+	return &CharInstance{int(charLength), collation}, nil
 }
 
 // CharInstance is the TypeInstance of Char.
 type CharInstance struct {
-	length    ranges.Int
-	collation sql.Collation
+	charLength int
+	collation  sql.Collation
 }
 
 var _ TypeInstance = (*CharInstance)(nil)
 
 // Get implements the TypeInstance interface.
 func (i *CharInstance) Get() (Value, error) {
-	n, err := i.length.RandomValue()
+	v, err := rand.StringExtendedAlphanumeric(i.charLength)
 	if err != nil {
 		return NilValue{}, errors.Wrap(err)
 	}
-	v, err := rand.String(int(n))
-	if err != nil {
-		return NilValue{}, errors.Wrap(err)
-	}
-	v = strings.TrimRightFunc(v, unicode.IsSpace)
-	return CharValue{StringValue(v), int(i.length.Upperbound)}, err
+	return CharValue{StringValue(v)}, err
 }
 
 // TypeValue implements the TypeInstance interface.
 func (i *CharInstance) TypeValue() Value {
-	return CharValue{StringValue(""), int(i.length.Upperbound)}
+	return CharValue{StringValue("")}
 }
 
 // Name implements the TypeInstance interface.
 func (i *CharInstance) Name(sqlite bool) string {
 	if sqlite {
-		return fmt.Sprintf("CHAR(%d)", i.length.Upperbound)
+		return fmt.Sprintf("CHAR(%d)", i.charLength)
 	}
-	return fmt.Sprintf("CHAR(%d) COLLATE %s", i.length.Upperbound, i.collation.String())
+	return fmt.Sprintf("CHAR(%d) COLLATE %s", i.charLength, i.collation.String())
 }
 
 // MaxValueCount implements the TypeInstance interface.
 func (i *CharInstance) MaxValueCount() float64 {
-	return math.Pow(float64(rand.StringCharSize()), float64(i.length.Upperbound))
+	return math.Pow(float64(rand.StringExtendedAlphanumericCharSize()), float64(i.charLength))
 }
 
 // CharValue is the Value type of a CharInstance.
 type CharValue struct {
 	StringValue
-	charLength int
 }
 
 var _ Value = CharValue{}
